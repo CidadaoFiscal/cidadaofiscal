4574e00959cf4b291cbf3371b622a1afd6e2f644
Saulo <scadete@gmail.com>
2018-04-13 00:02:08 -0300

    Adds detail page routing and first content version

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { AboutComponent } from './about/about.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'details', component: DetailComponent },
+  { path: 'detail', component: DetailComponent },
   { path: 'about', component: AboutComponent }
 ];
 
