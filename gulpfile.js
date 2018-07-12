0b690f397bd74c3c7444eb36a4fd3b7757e036e0
Saulo <1699816+scadete@users.noreply.github.com>
2018-07-12 20:10:56 -0300

    Adds gulp task to copy api files to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 
 
 gulp.task('client-dist-copy', function() {
-  gulp.src('client/dist/*')
+  gulp.src('client/dist/**')
   .pipe(gulp.dest('dist'))
+});
+
+gulp.task('api-dist-copy', function() {
+  gulp.src('rest-api/**')
+  .pipe(gulp.dest('dist/api'))
 });
 
 gulp.task('default', function() {
