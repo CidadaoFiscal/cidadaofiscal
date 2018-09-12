a8b60e5ba87a009e0413bb983b67313617ce0f9a
Saulo <1699816+scadete@users.noreply.github.com>
2018-09-12 20:59:09 -0300

    Improves database bootstrap feedback

--- database/bootstrap-database.py
+++ database/bootstrap-database.py
@@ -2,9 +2,22 @@
 import mysql.connector as mariadb
 import csv
 
-mariadb_connection = mariadb.connect(user='cfuser', password='cfs3cr3t', database='cidadaofiscal', port='38306')
+DB_USER = 'cfuser'
+DB_PASS = 'cfs3cr3t'
+DB_NAME = 'cidadaofiscal'
+DB_HOST = '127.0.0.1'
+DB_PORT = '38306'
+
+COMMIT_STEP = 100
+
+print('Connecting to {0}@{1}:{2}/{3}...'.format(DB_USER, DB_HOST, DB_PORT, DB_NAME))
+
+mariadb_connection = mariadb.connect(user=DB_USER, password=DB_PASS, database=DB_NAME, host=DB_HOST, port=DB_PORT)
 cursor = mariadb_connection.cursor()
 
+print('Cleaning previous data...')
+cursor.execute("""DROP TABLE IF EXISTS cf_alepe""")
+print('Creating schema...')
 cursor.execute("""CREATE TABLE IF NOT EXISTS cf_alepe (
                 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 parlamentar_fantasia VARCHAR(255),
@@ -35,12 +48,24 @@ cursor.execute("CREATE INDEX IF NOT EXISTS cf_alepe_idx_despesa_canceleada ON cf
 cursor.execute("CREATE INDEX IF NOT EXISTS cf_alepe_idx_fornecedor_id ON cf_alepe (fornecedor_id)")
 cursor.execute("CREATE INDEX IF NOT EXISTS cf_alepe_idx_fornecedor_nome ON cf_alepe (fornecedor_nome)")
 
-with open('cf_alepe_data.csv', 'rt') as csvfile:
+print('Loading data...')
+
+with open('basecompleta-clear.csv', 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+    rowCount = len(list(spamreader))
+    csvfile.seek(0)
+    currentRow = 0
     isDataRow = False
     for row in spamreader:
+        currentRow = currentRow + 1
         if isDataRow:
-            cursor.execute("""INSERT INTO cf_alepe (parlamentar_fantasia, 
+            if ((currentRow % COMMIT_STEP == 0) or (currentRow == rowCount)):
+                print ('{0:.2f}% : {1}/{2}'.format((100*currentRow/rowCount), currentRow, rowCount), end='\r')
+                mariadb_connection.commit()
+
+            cursor.execute("""INSERT INTO cf_alepe (
+                parlamentar_fantasia, 
                 parlamentar_partido, 
                 parlamentar_nome, 
                 parlamentar_dtnasc, 
@@ -58,7 +83,9 @@ with open('cf_alepe_data.csv', 'rt') as csvfile:
                 VALUES (
                     %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
                 )""", tuple(row))
-        isDataRow = True;
+        isDataRow = True
+    print ('\nDone updating database.')
 cursor.close()
 mariadb_connection.commit()
 mariadb_connection.close()
+print ('Database connection closed.')
